eb5abd90ea9b2f32c2981a1134b9e54bf5df8c54
kosty <kostyanidze173@mail.ru>
2024-10-27 15:57:51 +0400

    Default

--- pmu_flutter/lib/main.dart
+++ pmu_flutter/lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Шаляхин К.С. ЦПИбв-41',
+      title: 'My Test App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Шаляхин К.С. ЦПИбв-41'),
+      home: const MyHomePage(title: 'My Test App'),
     );
   }
 }
